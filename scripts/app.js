9568ebf2fe3824a4af82c8d7b9e6ce05c3ee4bf5
jameshaddrill <jameshaddrill@gmail.com>
2015-08-11 16:33:22 +0100

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -6,28 +6,84 @@ var	sounds = {
 		1 : 0,
 		2 : 0,
 		3 : 0,
-		4 : 0
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
 	},
 	osc2 : {
 		0 : 0,
 		1 : 0,
 		2 : 0,
 		3 : 0,
-		4 : 0
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
 	},
 	osc3 : {
 		0 : 0,
 		1 : 0,
 		2 : 0,
 		3 : 0,
-		4 : 0
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
 	},
 	osc4 : {
 		0 : 0,
 		1 : 0,
 		2 : 0,
 		3 : 0,
-		4 : 0
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
+	},
+	osc5 : {
+		0 : 0,
+		1 : 0,
+		2 : 0,
+		3 : 0,
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
+	},
+	osc6 : {
+		0 : 0,
+		1 : 0,
+		2 : 0,
+		3 : 0,
+		4 : 0,
+		5 : 0,
+		6 : 0,
+		7 : 0,
+		8 : 0,
+		9 : 0,
+		10 : 0,
+		11 : 0
 	}
 }
 
@@ -36,6 +92,16 @@ sounds2 = [0,0,0,0,0];
 
 window.addEventListener('load', init, false);
 function init() {
+
+	$('.inputTrigger').click( function() {
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+		} else {
+			$(this).addClass('active');
+		}
+		$(this).find('input').trigger('click');
+	});
+
 	try {
 		// Fix up for prefixing
 		window.AudioContext =
@@ -48,20 +114,23 @@ function init() {
 
 	oscillator1();
 	oscillator2();
+	oscillator3();
+	oscillator4();
+	oscillator5();
+	oscillator6();
 
 	$('.start').click(function() {
 		playPattern();
 	});
 
-	$('.stop').click(function() {
-		gainNode.gain.value = 0;
-	});
 
-	$('input[type="checkbox"').click( function(){
+
+	$('input[type="checkbox"]').click( function(){
 		var pos = parseFloat($(this).attr('value'));
 		//var rowNo = parseFloat($(this).parent('div').attr('id'));
-		var rowNo = "osc" + parseFloat($(this).parent('div').attr('id'));
+		var rowNo = "osc" + parseFloat($(this).parents('.inputRow').attr('id'));
 		var base = sounds[rowNo];
+		console.log(base);
 
 		if($(this).is(':checked')) {
 			base[pos] = 1;
@@ -73,9 +142,13 @@ function init() {
 }
 
 function playPattern() {
-	for(i=0; i < 7; i++) {
+	for(i=0; i < 13; i++) {
 		play(i);
 	};
+	setTimeout(function(){
+		i = 0;
+		playPattern();
+	}, 2400);
 
 	function play(i) {
 		setTimeout(function() {
@@ -90,8 +163,36 @@ function playPattern() {
 			} else {
 				gainNode2.gain.value = 0;
 			}
+
+			if (sounds.osc3[i] === 1) {
+				gainNode3.gain.value = .2;
+			} else {
+				gainNode3.gain.value = 0;
+			}
+
+			if (sounds.osc4[i] === 1) {
+				gainNode4.gain.value = .2;
+			} else {
+				gainNode4.gain.value = 0;
+			}
+
+			if (sounds.osc5[i] === 1) {
+				gainNode5.gain.value = .2;
+			} else {
+				gainNode5.gain.value = 0;
+			}
+
+			if (sounds.osc6[i] === 1) {
+				gainNode6.gain.value = .2;
+			} else {
+				gainNode6.gain.value = 0;
+			}
+
 		}, 200*i);
+
+
 	};
+
 };
 
 function oscillator1() {
@@ -111,7 +212,7 @@ function oscillator1() {
 function oscillator2() {
 	var oscillator2 = context.createOscillator();
 	oscillator2.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-	oscillator2.frequency.value = 600; // value in hertz
+	oscillator2.frequency.value = 300; // value in hertz
 
 	gainNode2 = context.createGain();
 	oscillator2.connect(gainNode2);
@@ -120,4 +221,57 @@ function oscillator2() {
 	gainNode2.gain.value = 0;
 
 	oscillator2.start();
+};
+
+function oscillator3() {
+	var oscillator3 = context.createOscillator();
+	oscillator3.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	oscillator3.frequency.value = 400; // value in hertz
+
+	gainNode3 = context.createGain();
+	oscillator3.connect(gainNode3);
+
+	gainNode3.connect(context.destination);
+	gainNode3.gain.value = 0;
+
+	oscillator3.start();
+};
+
+function oscillator4() {
+	var oscillator4 = context.createOscillator();
+	oscillator4.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	oscillator4.frequency.value = 500; // value in hertz
+	gainNode4 = context.createGain();
+	oscillator4.connect(gainNode4);
+
+	gainNode4.connect(context.destination);
+	gainNode4.gain.value = 0;
+
+	oscillator4.start();
+};
+
+function oscillator5() {
+	var oscillator5 = context.createOscillator();
+	oscillator5.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	oscillator5.frequency.value = 600; // value in hertz
+	gainNode5 = context.createGain();
+	oscillator5.connect(gainNode5);
+
+	gainNode5.connect(context.destination);
+	gainNode5.gain.value = 0;
+
+	oscillator5.start();
+};
+
+function oscillator6() {
+	var oscillator6 = context.createOscillator();
+	oscillator6.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	oscillator6.frequency.value = 800; // value in hertz
+	gainNode6 = context.createGain();
+	oscillator6.connect(gainNode6);
+
+	gainNode6.connect(context.destination);
+	gainNode6.gain.value = 0;
+
+	oscillator6.start();
 };
