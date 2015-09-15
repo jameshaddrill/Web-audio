ec992940ee7251ef74ecea5cddf16bd902652532
jameshaddrill <jameshaddrill@gmail.com>
2015-09-15 22:37:05 +0100

    Working with Leap motion

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,6 +1,9 @@
 // http://noisehack.com/generate-noise-web-audio-api/#pink-noise
 // http://noisehack.com/custom-audio-effects-javascript-web-audio-api/
 
+$(document).ready(function() {
+});
+
 var context;
 
 window.addEventListener('load', init, false);
@@ -9,39 +12,100 @@ function init() {
 	try {
 		// Fix up for prefixing
 		window.AudioContext =
-	  window.AudioContext||window.webkitAudioContext;
-	  	context = new AudioContext();
+		window.AudioContext||window.webkitAudioContext;
+		context = new AudioContext();
 	}
 	catch(e) {
 		alert('Web Audio API is not supported');
 	}
 
 	var oscillator = context.createOscillator();
-	oscillator.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-	oscillator.frequency.value = 200; // value in hertz
+	oscillator.type = 'square'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	//oscillator.frequency.value = 200; // value in hertz
 
 	gainNode = context.createGain();
 	oscillator.connect(gainNode);
 
 	gainNode.connect(context.destination);
-	gainNode.gain.value = 10;
+	gainNode.gain.value = 5;
 
 	oscillator.start();
+	
+	Leap.loop({enableGestures: true}, function(frame) { // Constantly updates
+		console.log(frame); // Frame contains all currently available data
+		//var fps = frame.currentFrameRate;
+		frame.currentFrameRate = 5;
+		
+		
+		if(frame.hands.length > 0)
+		{
+			var hand = frame.hands[0];
+			console.log(frame.hands.type)
+			console.log(hand.stabilizedPalmPosition[1]);
+			var position = hand.stabilizedPalmPosition[1];
+			var velocity = hand.palmVelocity;
+			var direction = hand.direction;
+			var x = 0;
+			hand.fingers.forEach(function(finger){
+				x = x + 1;
+				//setVol(x);
+			});
+		
+			position = (parseFloat(position));
+			
+			position = Math.floor(position.toFixed(0));
+			
+			position = position + 20;
+			
+			
+			if (position < 20) {
+				position = 20;
+			}
+			
+			refreshTone(position);
+		} else {
+			var position = 0 
+			
+			refreshTone(position);
+		}
+		
+		
+		
+		
+	});
+		
+		var refreshTone = function(position) {
+			//$('#position').html(position);
+			
+			
+			
+			//console.log(position);
+			pitchControl = parseFloat(position) * 2; //parseFloat(currentMousePos.x);
 
-	var currentMousePos = { x: -1, y: -1 };
+				oscillator.frequency.value = pitchControl;
 
-	$(document).mousemove(function(event) {
-        currentMousePos.x = event.pageX;
-        currentMousePos.y = event.pageY;
+			
+			//console.log(position, pitchControl);
+				
+			//console.log(oscillator.frequency.value);
 
-        $('.value').html(currentMousePos.y)
-
-        volControl = parseFloat(currentMousePos.y)  / 3000;
-        pitchControl = parseFloat(currentMousePos.x);
-
-        gainNode.gain.value = volControl;
-        oscillator.frequency.value = pitchControl;
-    });
+		}
+		
+		var setVol = function(vol) {
+		console.log(vol[2]);
+				vol = parseFloat(vol);
+		
+				volControl = 0.25 * vol;
+				
+				if (volControl > 0) {
+					gainNode.gain.value = volControl;
+				} else {
+					gainNode.gain.value = 0;
+				}
+				
+				
+				//console.log(volControl);
+			};
 }
 /*
 var bufferSize = 4096;
